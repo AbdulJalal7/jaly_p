507ae4c62b70233e25fdb8cc1ee8ea143f3f16e2
jalaludain7 <jalaludain7@gmail.com>
2026-03-31 18:27:29 +0500

    eas config

---
+++ app/index.jsx
@@ -0,0 +1,8 @@
+import { Redirect } from 'expo-router';
+
+export default function Index() {
+  // This file acts as the entry point for the root '/' route.
+  // The global redirect logic in app/_layout.jsx will handle 
+  // moving the user to either /(auth)/login or /(tabs)/home.
+  return null;
+}
